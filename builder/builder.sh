082f95c285c996b0023ba64c658f7efba10ce48d
MIDO <155222995+Midohajhouj@users.noreply.github.com>
2025-03-23 02:09:44 -0400

    DEBIAN Build

--- builder/builder.sh
+++ builder/builder.sh
@@ -2,6 +2,8 @@
 
 # Build the .deb package
 echo "Building .deb package..."
+cd ddos-toolkit/DEBIAN
+chmod +x * && cd .. && cd ..
 dpkg-deb --build ddos-toolkit
 
 echo "Package built successfully: ddos-toolkit.deb"
