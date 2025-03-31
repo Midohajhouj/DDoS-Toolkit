59288d80f5c1080f6ebad0549ba1e7105b74c02e
MIDO <155222995+Midohajhouj@users.noreply.github.com>
2025-03-31 01:09:08 -0400

    Delete builder directory

--- builder/builder.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Easter egg LIONMAD salut u  
-# Build the .deb package
-echo "Building .deb package..."
-cd ddos-toolkit/DEBIAN
-chmod +x * && cd .. && cd ..
-dpkg-deb --build ddos-toolkit
-
-echo "Package built successfully: ddos-toolkit.deb"
